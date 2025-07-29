2a407aa8b3cf6e7830f836242021a23bb2b0ac1b
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-07-29 17:24:39 +0100

    add base path to vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // Proxy API requests during development to Django backend
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/django-react-app/react-app',
+  base: process.env.VITE_BASE_URL || '/react-book-app',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
